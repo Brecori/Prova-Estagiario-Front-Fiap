38d5e5cfd8afcf9ff9d2a6b52faf6164f7ad519e
Brecori <breno_tosi@hotmail.com>
2023-01-21 03:09:58 -0300

    Header Finalizado | Main Finalizada | Início  Seções

---
+++ app/main.js
@@ -0,0 +1,19 @@
+const nav = document.querySelector("#nav");
+
+window.onscroll = function() {
+    scroll();
+}
+
+function scroll() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        nav.classList.remove("bg-header-off");
+        nav.classList.add("bg-header-on");
+    }
+    else {
+        nav.classList.remove("bg-header-on");
+        nav.classList.add("bg-header-off");
+    }
+}
+
+
+
